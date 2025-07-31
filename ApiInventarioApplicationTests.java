3c6695b54256133a12952cfb3fe99918f742c2ef
davidMoreno16 <cdmoreno3@utpl.edu.ec>
2025-07-30 21:33:38 -0500

    Create ApiInventarioApplicationTests.java

---
+++ ApiInventarioApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ejemplo.api_inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInventarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Verifica que el contexto de la aplicación se carga correctamente
+    }
+
+    // Puedes agregar más pruebas aquí
+}
